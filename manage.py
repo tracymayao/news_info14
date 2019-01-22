902e5196b935633eef30d4af6d1415a614318c18
tracymayao <2514249126@qq.com>
2019-01-22 19:09:04 +0800

    new_case

--- manage.py
+++ manage.py
@@ -1,9 +1,9 @@
-from flask_migrate import MigrateCommand,Migrate
+from flask_migrate import MigrateCommand, Migrate
 
 # 使用管理器
 from flask_script import Manager
 # 导入工厂函数
-from info import create_app,db,models
+from info import create_app, db, models
 
 # 调用__init__文件中的工厂函数，获取app
 from info.models import User
@@ -11,8 +11,8 @@ from info.models import User
 app = create_app('development')
 
 manage = Manager(app)
-Migrate(app,db)
-manage.add_command('db',MigrateCommand)
+Migrate(app, db)
+manage.add_command('db', MigrateCommand)
 
 
 # 创建管理员账户
@@ -36,6 +36,7 @@ def create_supper_user(name, password):
         db.session.rollback()
         print(e)
     print('管理员创建成功')
+
 
 if __name__ == '__main__':
     print(app.url_map)
